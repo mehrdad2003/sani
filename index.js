63caea737b441f28a72e06b42f8104aafa003f5d
mehrdadzare <62208360+mehrdad2003@users.noreply.github.com>
2023-10-29 09:37:45 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-'use strict';                                               
-const puppeteer = require('puppeteer');
-(async() => {
-const browser = await puppeteer.launch({executablePath: '/usr/bin/google-chrome-stable',headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox']});
-                                   
-const page = await browser.newPage();
-await page.goto('https://mehrdad2003.github.io/testy/1.html');      
-await page.waitForTimeout(700000)                                                    
-browser.close();
-})();
+const express = require('express'),
+    app = express(),
+    puppeteer = require('puppeteer');
+
+app.get("/", async (request, response) => {
+  try {
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox']
+    });
+    const page = await browser.newPage();
+    await page.goto('https://developers.google.com/web/tools/puppeteer');
+    await page.screenshot({path: __dirname+'/public/puppeteer.png'});
+    await browser.close();
+    response.sendFile(__dirname+'/public/puppeteer.png');
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
